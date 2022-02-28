deeddb836efce54db1e523ea9b30ba7721d6d8af
Candida Larson <candida-latson@mail.ru>
2022-02-28 19:27:43 +0300

    Add method printThreeWords into HomeWorkApp class

--- src/com/geek/java/HomeWorkApp.java
+++ src/com/geek/java/HomeWorkApp.java
@@ -3,7 +3,13 @@ package com.geek.java;
 public class HomeWorkApp {
 
     public static void main(String[] args) {
+        printThreeWords();
+    }
 
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
     }
 
 }
