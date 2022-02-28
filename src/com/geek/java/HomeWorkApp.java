c3241141c3bccf46ed35d4f426218f51e00d3f30
Candida Larson <candida-latson@mail.ru>
2022-02-28 19:57:19 +0300

    Revert "Add method printThreeWords into HomeWorkApp class"
    This reverts commit deeddb836efce54db1e523ea9b30ba7721d6d8af.

--- src/com/geek/java/HomeWorkApp.java
+++ src/com/geek/java/HomeWorkApp.java
@@ -3,13 +3,7 @@ package com.geek.java;
 public class HomeWorkApp {
 
     public static void main(String[] args) {
-        printThreeWords();
-    }
 
-    public static void printThreeWords(){
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
     }
 
 }
