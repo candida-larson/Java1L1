b3994ca2de15d4557ca82a0c2afc5aa3e787c829
Candida Larson <candida-latson@mail.ru>
2022-02-28 19:32:46 +0300

    Add method checkSumSign into HomeWorkApp class
    (cherry picked from commit 1d63e919c83e333eab6c8e5381b35ab4ad8c37b2)

--- src/com/geek/java/HomeWorkApp.java
+++ src/com/geek/java/HomeWorkApp.java
@@ -4,12 +4,24 @@ public class HomeWorkApp {
 
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 
     public static void printThreeWords(){
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+
+    public static void checkSumSign(){
+        int a = 11;
+        int b = -19;
+
+        if( a + b >= 0 ){
+            System.out.println("Сумма положительная");
+        }else{
+            System.out.println("Сумма отрицательная");
+        }
     }
 
 }
