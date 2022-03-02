df73eea1c85de89e928586c9725992b16a72de98
Candida Larson <candida-latson@mail.ru>
2022-03-02 23:33:01 +0300

    add feature #1

--- src/com/geek/java/Methods.java
+++ src/com/geek/java/Methods.java
@@ -1,10 +1,15 @@
 package com.geek.java;
 
 public class Methods {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.println("checkSum = " + checkSum(111, 6));
     }
 
+    private static boolean checkSum(int a, int b){
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
 
 
 }
