86f22f0bd772fd0fc98e456e0f257e837a1e4113
Candida Larson <candida-latson@mail.ru>
2022-03-02 23:36:50 +0300

    add feature #2

--- src/com/geek/java/Methods.java
+++ src/com/geek/java/Methods.java
@@ -4,11 +4,22 @@ public class Methods {
 
     public static void main(String[] args) {
         System.out.println("checkSum = " + checkSum(111, 6));
+        showSignOfNumber(-99);
     }
 
     private static boolean checkSum(int a, int b){
         int sum = a + b;
         return sum >= 10 && sum <= 20;
+    }
+
+    private static void showSignOfNumber(int number){
+
+        if( number >= 0 ){
+            System.out.println("Положительное");
+        }else {
+            System.out.println("Отрицательное");
+        }
+
     }
 
 
