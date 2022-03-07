f07b4f1af41f03a8788d014be3e7409e2aed7f7d
Candida Larson <candida-latson@mail.ru>
2022-03-07 23:18:20 +0300

    add revertArray method

--- src/com/geek/java/ArrayMethods.java
+++ src/com/geek/java/ArrayMethods.java
@@ -1,7 +1,25 @@
 package com.geek.java;
 
+import java.util.Arrays;
+
 public class ArrayMethods {
     public static void main(String[] args) {
 
+        System.out.println("Task #1:");
+        byte[] inputBytes = {0, 1, 1, 0, 1, 1};
+        System.out.println(Arrays.toString(revertArray(inputBytes)));
+
     }
+
+    private static byte[] revertArray(byte[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+        }
+        return arr;
+    }
+
 }
