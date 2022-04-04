a3b80193c79fcf17cee4cb050b7def64660fb781
Candida Larson <candida-latson@mail.ru>
2022-04-04 18:33:55 +0300

    Copy default implementation CounterApp

--- src/com/geek/java/GUICounter/CounterApp.java
+++ src/com/geek/java/GUICounter/CounterApp.java
@@ -1,9 +1,58 @@
 package com.geek.java.GUICounter;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CounterApp extends JFrame {
-    private int counter;
+    private int counter = 0;
+    private final JLabel counterValueView = new JLabel();
 
+    public CounterApp() {
+        setBounds(500, 500, 300, 180);
+        setTitle("CounterApp");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        Font font = new Font("Tahoma", Font.BOLD, 36);
+
+        counterValueView.setFont(font);
+        counterValueView.setHorizontalAlignment(SwingConstants.CENTER);
+        add(counterValueView, BorderLayout.CENTER);
+        counterValueView.setText(String.valueOf(counter));
+
+        JButton decrementButton = new JButton("<");
+        decrementButton.setFont(font);
+        add(decrementButton, BorderLayout.WEST);
+
+        JButton incrementButton = new JButton(">");
+        incrementButton.setFont(font);
+        add(incrementButton, BorderLayout.EAST);
+
+        decrementButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                counter--;
+                counterValueView.setText(String.valueOf(counter));
+            }
+        });
+
+        incrementButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                counter++;
+                counterValueView.setText(String.valueOf(counter));
+            }
+        });
+
+        setVisible(true);
+    }
+
+    private void changeCounter(int offset){
+        counter += offset;
+    }
+
+    public static void main(String[] args) {
+        new CounterApp();
+    }
 }
