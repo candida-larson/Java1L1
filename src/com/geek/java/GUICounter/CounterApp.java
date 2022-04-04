ed97125f406f008db4cc38df8070c32e12938574
Candida Larson <candida-latson@mail.ru>
2022-04-04 19:14:46 +0300

    Complete all tasks
    Add Clear, "+10" and "-10" buttons

--- src/com/geek/java/GUICounter/CounterApp.java
+++ src/com/geek/java/GUICounter/CounterApp.java
@@ -8,48 +8,105 @@ import java.awt.event.ActionListener;
 public class CounterApp extends JFrame {
     private int counter = 0;
     private final JLabel counterValueView = new JLabel();
+    Font font = new Font("Tahoma", Font.BOLD, 36);
 
     public CounterApp() {
-        setBounds(500, 500, 300, 180);
-        setTitle("CounterApp");
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        configFrame();
+        configCounterValueView();
 
-        Font font = new Font("Tahoma", Font.BOLD, 36);
-
-        counterValueView.setFont(font);
-        counterValueView.setHorizontalAlignment(SwingConstants.CENTER);
-        add(counterValueView, BorderLayout.CENTER);
-        counterValueView.setText(String.valueOf(counter));
-
-        JButton decrementButton = new JButton("<");
-        decrementButton.setFont(font);
-        add(decrementButton, BorderLayout.WEST);
-
-        JButton incrementButton = new JButton(">");
-        incrementButton.setFont(font);
-        add(incrementButton, BorderLayout.EAST);
-
-        decrementButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                counter--;
-                counterValueView.setText(String.valueOf(counter));
-            }
-        });
-
-        incrementButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                counter++;
-                counterValueView.setText(String.valueOf(counter));
-            }
-        });
+        initIncrementButton();
+        initDecrementButton();
+        initClearButton();
+        initTenIncAndDecButton();
 
         setVisible(true);
     }
 
-    private void changeCounter(int offset){
+    private void configCounterValueView() {
+        counterValueView.setFont(font);
+        counterValueView.setHorizontalAlignment(SwingConstants.CENTER);
+        add(counterValueView, BorderLayout.CENTER);
+        counterValueView.setText(String.valueOf(counter));
+    }
+
+    private void initIncrementButton() {
+        JButton incrementButton = new JButton(">");
+        incrementButton.setFont(font);
+        add(incrementButton, BorderLayout.EAST);
+
+        incrementButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                changeCounterInView(1);
+            }
+        });
+    }
+
+    private void initDecrementButton() {
+        JButton decrementButton = new JButton("<");
+        decrementButton.setFont(font);
+        add(decrementButton, BorderLayout.WEST);
+
+        decrementButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                changeCounterInView(-1);
+            }
+        });
+    }
+
+    private void initClearButton() {
+        JButton clearButton = new JButton("Clear");
+        clearButton.setFont(font);
+        add(clearButton, BorderLayout.NORTH);
+
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearCounter();
+            }
+        });
+    }
+
+    private void initTenIncAndDecButton() {
+        JButton tenIncButton = new JButton("+10");
+        tenIncButton.setFont(font);
+        tenIncButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                changeCounterInView(10);
+            }
+        });
+
+        JButton tenDecButton = new JButton("-10");
+        tenDecButton.setFont(font);
+        tenDecButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                changeCounterInView(-10);
+            }
+        });
+
+        Panel panel = new Panel();
+        panel.add(tenIncButton);
+        panel.add(tenDecButton);
+        add(panel, BorderLayout.SOUTH);
+    }
+
+    private void configFrame() {
+        setBounds(400, 400, 300, 240);
+        setTitle("CounterApp");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    private void clearCounter() {
+        counter = 0;
+        counterValueView.setText(String.valueOf(counter));
+    }
+
+    private void changeCounterInView(int offset) {
         counter += offset;
+        counterValueView.setText(String.valueOf(counter));
     }
 
     public static void main(String[] args) {
